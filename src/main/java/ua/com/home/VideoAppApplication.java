74c18a180116a9f0290aca5d518792a21bc6a949
bohdan <bshevyrov@gmail.com>
2022-03-19 17:16:46 +0200

    Initial commit

---
+++ src/main/java/ua/com/home/VideoAppApplication.java
@@ -0,0 +1,31 @@
+package ua.com.home;
+
+import inet.ipaddr.IPAddressString;
+import inet.ipaddr.ipv4.IPv4Address;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
+public class VideoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoAppApplication.class, args);
+        IPv4Address loopbackv4 = new IPAddressString("127.0.0.1").
+                getAddress().toIPv4();
+        int val = loopbackv4.intValue();
+        System.out.println(val);
+        IPv4Address backAgainv4 = new IPv4Address(val);
+        System.out.println(backAgainv4);
+    }
+
+}
---
+++ src/test/java/ua/com/home/VideoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
